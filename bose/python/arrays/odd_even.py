3c19f6a0b2431c8cf1d308ab0e7c715ad63927e7
Abhishek Bose <abose550@gmail.com>
2020-07-30 00:06:01 +0530

    push even to front

---
+++ bose/python/arrays/odd_even.py
@@ -0,0 +1,18 @@
+def reorder_odd_even(arr):
+    i=0
+    j=len(arr)-1
+    while i<j:
+        if arr[i]%2!=0:
+            if arr[j]%2==0:
+                arr[i],arr[j] = arr[j],arr[i]
+                i+=1
+            j-=1
+        else:
+            i+=1
+
+    return arr
+
+
+arr = [2,4,6,8,5,3,9,11]
+
+print(reorder_odd_even(arr))
