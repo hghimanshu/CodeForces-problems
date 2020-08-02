e4d7c1d2f72fced6a2457340b0706f9d5679a518
hghimanshu <hghimanshu81@gmail.com>
2020-08-02 20:39:07 +0530

    size of largets subtree

---
+++ BinaryTrees/sizeOfLargetSubtree.py
@@ -0,0 +1,54 @@
+class Tree:
+    def __init__(self, data=0, left=None, right=None):
+        self.data = data
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def calculateDepthatANode(self, root, ht):
+        if root:
+            if root.left is None and root.right is None:
+                return ht
+            ht +=1
+            ht = self.calculateDepthatANode(root.left, ht)
+            ht = self.calculateDepthatANode(root.right, ht)
+        return ht
+    
+    
+    def largestSubtree(self, root, array):
+        if root:
+            left_depth = self.calculateDepthatANode(root.left, 0)
+            right_depth = self.calculateDepthatANode(root.right, 0)
+
+            array.append(max(left_depth, right_depth))
+            self.largestSubtree(root.left ,array)
+            self.largestSubtree(root.right, array)
+        return max(array)
+
+
+    
+
+## [314, 6,6,271,561,28,0,None,3,17,None,2,271,None,1,None,28,401,257,None,641]
+
+root = Tree(314)
+root.left = Tree(6)
+root.right = Tree(6)
+root.left.left = Tree(271)
+root.left.right = Tree(561)
+root.left.left.left = Tree(28)
+root.left.left.right = Tree(0)
+root.left.right.right = Tree(3)
+root.left.right.right.left = Tree(17)
+
+root.right.left = Tree(2)
+root.right.right = Tree(271)
+root.right.left.right = Tree(1)
+root.right.right.right = Tree(28)
+root.right.left.right.left = Tree(401)
+root.right.left.right.right = Tree(257)
+root.right.left.right.left.right = Tree(641)
+
+
+ans = Solution()
+print(ans.largestSubtree(root, []))
