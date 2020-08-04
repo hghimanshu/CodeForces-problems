b2bb523adf48bbdc46f5d012807c8f6934428246
hghimanshu <hghimanshu81@gmail.com>
2020-08-04 12:42:27 +0530

    update the code

--- Arrays/dutchNationalFlag.py
+++ Arrays/dutchNationalFlag.py
@@ -1,21 +1,23 @@
 def sortArr(arr, n):
-    count_each_element = [(i, arr.count(i)) for i in arr]
-    count_each_element = list(set(count_each_element))
-    count_each_element = sorted(count_each_element, key= lambda x: x[0])
-
-    j = 0
-
-    for i in range(len(count_each_element)):
-        current_val, current_val_count = count_each_element[i]
-        
-        while current_val_count > 0:
-            arr[j] = current_val
-            j +=1
-            current_val_count -=1
+    pivot = arr[n]
+    smaller = 0
+    for i in range(len(arr)):
+        if arr[i] < pivot:
+            arr[i], arr[smaller] = arr[smaller], arr[i]
+            smaller +=1
+    
+    larger = len(arr) -1
+    for i in reversed(range(len(arr))):
+        if arr[i] < pivot:
+            break
+        elif arr[i] > pivot:
+            arr[i], arr[larger] = arr[larger], arr[i]
+            larger -=1
+    
 
     return arr
 
 
-arr = [0, 1, 1, 3, 3, 2, 1, 2, 1, 0, 0, 1] 
-pivot = 2
+arr = [0, 1, 1, 0, 0, 2, 1, 2, 1, 0, 0, 1] 
+pivot = 5
 print(sortArr(arr, pivot)) 
