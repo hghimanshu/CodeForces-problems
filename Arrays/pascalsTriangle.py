677948348cdd0d985a42b7f7cf74fd11a04a39e4
hghimanshu <hghimanshu81@gmail.com>
2020-08-06 01:41:36 +0530

    pascals triangle

---
+++ Arrays/pascalsTriangle.py
@@ -0,0 +1,11 @@
+
+def consutructPascalTriangle(n):
+    result = [[1] *(i+1) for i in range(n)]
+    for i in range(n):
+        for j in range(1, i):
+            result[i][j] = result[i-1][j-1] + result[i-1][j]
+    return result
+
+
+n = 4
+print(consutructPascalTriangle(n))
