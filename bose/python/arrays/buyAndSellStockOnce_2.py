a49885a36b7586fed94ed19f468ed5aefc771d99
Jayant Ratti <jayantratti@Jayants-MacBook-Air.local>
2020-08-29 16:14:00 +0530

    solved using extra array

--- bose/python/arrays/buyAndSellStockOnce_2.py
+++ bose/python/arrays/buyAndSellStockOnce_2.py
@@ -13,7 +13,6 @@ def getMaximumProfit(a,n):
         return 0
     
     buy_array=[0]*n
-    sell_array=[0]*n
 
     min_buy_seen = 0
     for i in range(0,n):
@@ -23,24 +22,13 @@ def getMaximumProfit(a,n):
         else:
             min_buy_seen=getMin(a[i],min_buy_seen)
             buy_array[i]=min_buy_seen
-
-
-    max_sell_seen = 0
-    for i in range(0,n):
-        if i==0:
-            sell_array[0]=a[i]
-            max_sell_seen=a[i]
-        else:
-            max_sell_seen=getMax(a[i],max_sell_seen)
-            sell_array[i]=max_sell_seen
         
     print(buy_array)
-    print(sell_array)
 
-    current_profit = 
+    max_profit =0 
     for i in range(0,n):
-        ConnectionRefusedErr
-    # return max_profit
+        max_profit = getMax(max_profit,a[i]-buy_array[i])
+    return max_profit
 
 if __name__ == "__main__":
     arr  = [310,315,275,295,260,270,290,230,255,250]
